d7bda3dca683d812603ecdb2d1c81f9618cd98a4
Dan Tran <d.k.tran@uq.edu.au>
2025-07-10 10:41:18 +1000

    fix(config): update mobileObservations URL to production

--- src/app.config.js
+++ src/app.config.js
@@ -1,13 +1,13 @@
 export const development = {
 	apiUrls: {
-		mobileObservations: 'https://f06kj1k332.execute-api.ap-southeast-2.amazonaws.com/dev',
+		mobileObservations: 'https://f06kj1k332.execute-api.ap-southeast-2.amazonaws.com/prod',
 		ddpDonations: 'https://c0z5ki2dea.execute-api.ap-southeast-2.amazonaws.com/prod/'
 	}
 };
 
 export const production = {
 	apiUrls: {
-		mobileObservations: 'https://f06kj1k332.execute-api.ap-southeast-2.amazonaws.com/dev',
+		mobileObservations: 'https://f06kj1k332.execute-api.ap-southeast-2.amazonaws.com/prod',
 		ddpDonations: 'https://c0z5ki2dea.execute-api.ap-southeast-2.amazonaws.com/prod/'
 	}
 };
